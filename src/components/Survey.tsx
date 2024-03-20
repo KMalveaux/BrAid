259087c6d516a4529554e75ae6d828ae7a39e26a
Reality Failure <Kevynjmalveaux@hotmail.com>
2024-03-20 16:20:52 -0500

    Added fetch request for JSON with survey questions

--- src/components/Survey.tsx
+++ src/components/Survey.tsx
@@ -1,14 +1,26 @@
-import { useEffect, useRef } from "react";
+// Third party library imports
+import { useEffect, useState, useRef } from "react";
+
+// Local imports
 import styles from "../css/Survey.module.css";
 
 interface showSurvey {
   onClose: () => void;
 }
 
+// The structure of our json file containing the survey questions
+interface JsonData {
+  QuestionOne: string;
+}
+
 // This component is passed an object of type showSurvey that only contains a function
 // that should return void. It is passed a function definition (in LandingPage.tsx)
 // that tells the showButton state to set itself to false.
 const Survey = ({ onClose }: showSurvey) => {
+  const [jsonData, setJsonData] = useState<JsonData | null>(null);
+
+  const answerArray = useRef<boolean[]>([]);
+
   useEffect(() => {
     document.body.style.overflow = "hidden";
     return () => {
@@ -16,7 +28,20 @@ const Survey = ({ onClose }: showSurvey) => {
     };
   }, []);
 
-  const answerArray = useRef<boolean[]>([]);
+  // The survey questions are served through a JSON file the public folder and are thus accessed through an asynchronous fetch request
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("../surveyQuestions.json");
+        const data = await response.json();
+        setJsonData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   function handleYesButtonClick(): void {
     answerArray.current.push(true);
@@ -34,12 +59,15 @@ const Survey = ({ onClose }: showSurvey) => {
           back
         </button>
         <div id={styles.surveyBody}>
-          <h1>Question one</h1>
-          <p>Hello</p>
-          <span>
-            <button onClick={handleYesButtonClick}>Yes</button>
-            <button onClick={handleNoButtonClick}>No</button>
-          </span>
+          {jsonData && (
+            <div>
+              <h1>{jsonData.QuestionOne}</h1>
+              <span>
+                <button onClick={handleYesButtonClick}>Yes</button>
+                <button onClick={handleNoButtonClick}>No</button>
+              </span>
+            </div>
+          )}
         </div>
       </div>
     </div>
